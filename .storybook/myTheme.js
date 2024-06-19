1815a1e44e648a38acd7a313975e8ac68533ee7d
jinmw <jinmw@rd.netease.com>
2024-06-19 10:12:57 +0800

    feat: change logo

--- .storybook/myTheme.js
+++ .storybook/myTheme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 export default create({
     base: 'light',
     brandTitle: 'Yodao UI',
-    brandUrl: 'https://zhixuan.youdao.com/',
-    brandImage: 'http://shared.ydstatic.com/ead/dynamic/dynamic-template/image/ow_adimg1/logo.png',
+    brandUrl: 'https://www.baidu.com/',
+    brandImage: 'https://shared.ydstatic.com/ead/temporaryUpload/icon-bg-preview.png',
     brandTarget: '_blank',
 });
